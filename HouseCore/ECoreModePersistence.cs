808fc80d8cf8b38b445fed89b73987ba745f7b15
David Moreno Magin <davidmorenomagin@icloud.com>
2018-07-25 23:59:46 +0200

    add more code

---
+++ HouseCore/ECoreModePersistence.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace es.dmoreno.house.core
+{
+    public enum ECoreModePersistence
+    {
+        /// <summary>
+        /// Core directly accesses the database
+        /// </summary>
+        DB = 0,
+
+        /// <summary>
+        /// Core accesses the data throught a restapi
+        /// </summary>
+        RestAPI = 1
+    }
+}
