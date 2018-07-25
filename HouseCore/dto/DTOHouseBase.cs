c11000ff0db592923f2d86f2b3df1177c322b26c
David Moreno Magin <davidmorenomagin@icloud.com>
2018-07-25 09:08:55 +0200

    develop

---
+++ HouseCore/dto/DTOHouseBase.cs
@@ -0,0 +1,14 @@
+﻿using es.dmoreno.utils.dataaccess.db;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace es.dmoreno.house.core.dto
+{
+    [TableAttributte]
+    public class DTOHouseBase
+    {
+        [FieldAttribute(FieldName = "id", IsAutoincrement = true, Type = ParamType.Int32, IsPrimaryKey = true)]
+        internal int ID { get; set; }
+    }
+}
---
+++ HouseCore/dto/DTOMaster.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace es.dmoreno.house.core.dto
+{
+    public class DTOMaster : DTOHouseBase
+    {
+        
+    }
+}
