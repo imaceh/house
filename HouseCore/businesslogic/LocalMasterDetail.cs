7139d92b3fec14f9e42d9deec31d178b111830bd
David Moreno Magin <davidmorenomagin@icloud.com>
2018-08-17 09:41:33 +0200

    develop

--- HouseCore/businesslogic/LocalMasterDetail.cs
+++ HouseCore/businesslogic/LocalMasterDetail.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using es.dmoreno.house.core.interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace es.dmoreno.house.core.businesslogic
 {
-    class MasterDetail
+    internal class LocalMasterDetail : IMasterDetail
     {
     }
 }
